b569ad0b0f7fcd5163e15efd52ef6d326c3088be
sadraict <sadra_ict@yahoo.com>
2013-11-19 15:08:02 +0330

    Update js.php
    Add Ajax to share plus

--- views/default/share/js.php
+++ views/default/share/js.php
@@ -8,3 +8,45 @@ elgg.ui.sharePopupHandler = function(hook, type, params, options) {
 };
 
 elgg.register_hook_handler('getOptions', 'ui.popup', elgg.ui.sharePopupHandler);
+
+elgg.provide('elgg.share');
+
+elgg.share.init = function() {
+
+};
+elgg.share.share = function(guid) {
+	button=$("#sharebutton"+guid);
+	count=$("#sharecount"+guid);	
+	elgg.action('share/share', {	
+		data: {
+			guid: guid
+		}, 
+		success: function(json) {
+			if ($(json.output).children('guid:first').length>0) {    //having "guid" means shareing succeeds
+				button.parent().html($(json.output).children('button:first').html());
+				count.parent().html($(json.output).children('count:first').html());			
+			}
+		}
+	});
+	return;
+};
+
+elgg.share.delete = function(guid) {
+	button=$("#sharebutton"+guid);
+	count=$("#sharecount"+guid);	
+	elgg.action('share/delete', {
+		data: {
+			guid: guid
+		}, 
+		success: function(json) {
+			if ($(json.output).children('guid:first').length>0) {
+				button.parent().html($(json.output).children('button:first').html());
+				count.parent().html($(json.output).children('count:first').html());			
+			}			
+		}
+	});
+	return;
+};
+
+elgg.register_hook_handler('init', 'system', elgg.share.init);
+
